3520b60c672b2277e6bad3bcf236a7818f757c5d
Kushina Atori <cjacksonsheppard@yahoo.com>
2022-06-19 21:54:09 -0500

    Update help.js

--- Pre-Built/Commands/help.js
+++ Pre-Built/Commands/help.js
@@ -1,1 +1,10 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
 
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('help')
+		.setDescription('If you need help.'),
+	async execute(interaction) {
+		return interaction.reply('If you need help please contact an admin.');
+	},
+};
